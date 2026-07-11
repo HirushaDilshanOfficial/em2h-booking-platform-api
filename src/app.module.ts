64e7b63cff4d5f27f22c11460af3f6306217a5cf
HirushaDilshanOfficial <hirushadilshanprofile@gmail.com>
2026-07-12 01:17:39 +0530

    feat: add user register and login module implementations

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './modules/users/users.module';
 import { BookingsModule } from './modules/bookings/bookings.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, BookingsModule],
+  imports: [PrismaModule, UsersModule, BookingsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
